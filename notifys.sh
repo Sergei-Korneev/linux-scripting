4557ae855801c807e2c214310a5ff41e43be120a
s.m.korneev@yandex.ru <sergei@pop-os.localdomain>
2021-02-23 21:47:03 +0500

    changed something

--- notifys.sh
+++
@@ -1,6 +0,0 @@
-#Memory
-if [[ $(cat /proc/meminfo | grep MemAvailable | awk '{ printf "%.0f" ,$2/1024 }') -lt 3000 ]]; then paplay '/home/sergei/mysounds/memorylow.ogg' ;fi
-
-#Temp 
-
-if [[ $(cat /sys/class/thermal/thermal_zone*/temp | awk 'NR == 1' |awk '{ printf "%.0f" ,$1/1000}') -gt 70 ]]; then paplay '/home/sergei/mysounds/temp_is_above70.ogg' ;fi
